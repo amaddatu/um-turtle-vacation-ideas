225c34addd4911aab2091850496c7678902c1b8d
amaddatu <>
2022-08-06 16:06:56 -0400

    Fix visual interaction with form

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,6 +1,24 @@
+import React, {useState} from 'react';
 import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../utils/mutations';
 
 const LoginForm = () => {
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  const [formState, setFormState] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  }
+
   const onFormSubmit = async(event) => {
     event.preventDefault();
     console.log("working");
@@ -9,11 +27,19 @@ const LoginForm = () => {
     <form onSubmit={onFormSubmit}>
       <div className="input-group mb-3">
         <span className="input-group-text" id="email-label">Email</span>
-        <input type="text" className="form-control" placeholder="Email" ariaLabel="Email" ariaDescribedby="email-label" />
+        <input 
+          name="email" type="text" className="form-control" placeholder="Email" ariaLabel="Email" ariaDescribedby="email-label" 
+          value={formState.email}
+          onChange={handleChange}
+          />
       </div>
       <div className="input-group mb-3">
         <span className="input-group-text" id="password-label">Password</span>
-        <input type="password" className="form-control" placeholder="Password" ariaLabel="Password" ariaDescribedby="password-label" />
+        <input 
+          name="password" type="password" className="form-control" placeholder="Password" ariaLabel="Password" ariaDescribedby="password-label" 
+          value={formState.password}
+          onChange={handleChange}
+        />
       </div>
     </form>
   );
